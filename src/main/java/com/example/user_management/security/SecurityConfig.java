232ef5ac68e8005c8ad8a4c98bb6337b61641cf5
Adarsh Yadav <yadavadarsh96k@gmail.com>
2026-01-01 16:20:41 +0530

    Fix 403 error lets see what will be the result and done

--- src/main/java/com/example/user_management/security/SecurityConfig.java
+++ src/main/java/com/example/user_management/security/SecurityConfig.java
@@ -1,21 +1,15 @@
 package com.example.user_management.security;
 
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 
 @Configuration
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable()) // disable CSRF
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/**").permitAll() // allow all /api/** endpoints
-                ); // optional
 
-        return http.build();
+    // Disable Spring Security completely
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/**");
     }
 }
