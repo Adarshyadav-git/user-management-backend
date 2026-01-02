c066010f52d7c4c22a95b3e6ec9e90e3c75fc3d3
Adarsh Yadav <yadavadarsh96k@gmail.com>
2026-01-02 11:57:35 +0530

    final 403 issue fix lets check it and redo is not runned

--- src/main/java/com/example/user_management/controller/HealthController.java
+++ src/main/java/com/example/user_management/controller/HealthController.java
@@ -4,11 +4,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HomeController {
+public class HealthController {
 
     @GetMapping("/")
     public String home(){
         return "Application is running on Railway ";
 
+    }
+    @GetMapping("/health")
+    public String health(){
+        return "OK";
     }
 }
